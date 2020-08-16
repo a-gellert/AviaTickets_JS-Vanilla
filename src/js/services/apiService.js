20e5dd34036d16f046890e020089921559be5f71
Александр Геллерт <sever6131@ya.ru>
2020-08-16 14:05:55 +0600

    get responses

---
+++ src/js/services/apiService.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+import config from '../config/apiConfig';
+
+class Api {
+    constructor(config) {
+        this.url = config.url;
+    }
+    async countries() {
+        try {
+            const response = await axios.get(`${this.url}/countries`);
+            return response.data;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+    async cities() {
+        try {
+            const response = await axios.get(`${this.url}/cities`);
+            return response.data;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
+    prices(params) {
+
+    }
+}
+
+const api = new Api(config);
+
+export default api;
